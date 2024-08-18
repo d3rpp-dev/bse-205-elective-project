f1178902b010ffebbe9870af07d10926757455fb
d3rpp <github@d3rpp.dev>
2024-08-19 00:53:43 +1200

    made the secret key file upload somewhat functional

--- src/lib/components/ui/input/index.ts
+++ src/lib/components/ui/input/index.ts
@@ -1,7 +1,9 @@
 import Root from "./input.svelte";
+import File from "./input-file.svelte";
 
 export {
 	Root,
+    File,
 	//
 	Root as Input,
 };
--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -3,12 +3,23 @@
 
 	import { Button } from "@/ui/button";
 	import { Label } from "@/ui/label";
-	import { Input } from "@/ui/input";
+	import { Input, File } from "@/ui/input";
 	import * as Card from "@/ui/card";
 
 	import { PAGE_TRANSITION_TIME } from "$lib";
 
 	import { fade } from "svelte/transition";
+
+    let username = $state("");
+    let password = $state("");
+    let secret_key_filelist: FileList | undefined = $state(undefined);
+
+    $effect(() => {
+        if (secret_key_filelist && secret_key_filelist.item(0)) {
+            const blob_url = URL.createObjectURL(secret_key_filelist.item(0)!);
+            fetch(blob_url).then(res => res.json()).then(console.log);
+        }
+    });
 
 	const onsubmit = (ev: SubmitEvent) => {
 		ev.preventDefault();
@@ -29,20 +40,34 @@
 					<div class="grid w-full items-center gap-4">
 						<div class="flex flex-col space-y-1.5">
 							<Label for="username">Username</Label>
-							<Input id="username" type="text" />
+							<Input id="username" type="text" bind:value={username} />
 						</div>
 
 						<div class="flex flex-col space-y-1.5">
 							<Label for="password">Password</Label>
-							<Input id="password" type="password" />
-						</div>
+							<Input id="password" type="password" bind:value={password} />
+						</div>  
 
 						<div class="flex flex-col space-y-1.5">
 							<Label for="secret-key">Secret Key</Label>
-							<Input
+							<File
 								id="secret-key"
 								class="cursor-pointer"
-								type="file"
+                                accept="application/json"
+
+                                onchange={(ev) => {
+                                    if (!(ev.target instanceof HTMLInputElement)) return;
+                                    if (ev.target.files) secret_key_filelist = ev.target.files;
+                                }}
+
+                                ondrop={(ev) => {
+                                    // handle drag and drop.
+                                    ev.stopPropagation();
+                                    ev.preventDefault();
+
+                                    const dt = ev.dataTransfer;
+                                    if (dt && dt.files) secret_key_filelist = dt.files;
+                                }}
 							/>
 						</div>
 
