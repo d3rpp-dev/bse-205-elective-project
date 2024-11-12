68c9cc775789dfbf718bcd77bb68217059b08da7
JSpring0303 <springthing0303@gmail.com>
2024-11-13 00:17:52 +1300

    landingHeaderPhase1

--- src/routes/(app)/app/+page.svelte
+++ src/routes/(app)/app/+page.svelte
@@ -1,14 +1,15 @@
 <script>
 	import Main from "@/main.svelte";
 
-	// import { page } from "$app/stores";
+	import { page } from "$app/stores";
 
 	import { Button } from "$lib/components/ui/button";
-	// import { trpc } from "$lib/trpc/client";
+	import { trpc } from "$lib/trpc/client";
 
 	import { toast } from "svelte-sonner";
 
-	// const rpc = trpc($page);
+	const rpc = trpc($page);
+	// const greeter = rpc.greeter.greeting.createQuery({name: "Jacksonplusten"});
 
 	const aaaa = () => {
 		toast.promise(new Promise((res) => setTimeout(res, 5_000)), {
@@ -34,6 +35,16 @@
 			<Button variant="destructive">Bills</Button>
 		</div>
 	</div>
+
+	<!-- {#if $greeter.isLoading}
+	loading
+	{:else if $greeter.isError}
+	is Error
+	{:else}
+	{$greeter.data}
+	{/if} -->
+
+
 
 	<Button onclick={aaaa} variant="default">Button</Button>
 	<Button variant="destructive">AAAAAAAA</Button>
