cd6736bb39d0423671ae1834afa4f5a47e45f3a7
JSpring0303 <86695974+JSpring0303@users.noreply.github.com>
2024-11-15 09:49:00 +1300

    landingHeaderPhase2 (#6)

--- src/lib/components/headers/landing-header.svelte
+++ src/lib/components/headers/landing-header.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
 	import type { User } from "lucia";
 	import { APPLICATION_NAME, APPLICATION_ICON } from ".";
-
 	import { AccountHeaderComponent } from "@/account";
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+
+	import LogoGreen from "$lib//logos/Logo-Green.svg"
 
 	interface Props {
 		user: User | null;
 	}
 
 	const { user }: Props = $props();
+
+	//need to add pathways for each page
+	const isHomePage = derived(page, $page => $page.url.pathname === '/');
+	const isUploadPage = derived(page, $page => $page.url.pathname === '/');
+	const isProfilePage = derived(page, $page => $page.url.pathname === '/src/routes/(app)/app/account/page.svelte');
+
 </script>
 
-<header class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+
+<header class="sticky top-0 z-50 w-full border-b border-border/40 bg-[#373737] backdrop-blur supports-[backdrop-filter]:bg-[#373737]"
 >
 
 	<div class="container mx-auto flex h-24 max-w-screen-2xl items-center px-4" >
 
 		<div class="flex items-center md:mr-4">
-
-			<!-- This is apparently svelte 5, this is cursed -->
 			
-			
-			<img src="src/lib/logos/Logo-Green.svg" alt="JailBird Green" >
+			<img src={LogoGreen} alt="JailBird Green" class="h-20">
 
-			<img src="src/lib/logos/JailBirdText.svg" alt="JailBird">
-
-			<span class="ml-4 h-6 text-lg font-bold sm:inline-block font-futura-demi" >
-				{APPLICATION_NAME}
-			</span>
+			<h1 class="">JailBird</h1>
 
 
 		</div>
@@ -37,22 +40,52 @@
 			<AccountHeaderComponent {user} />
 
 			<!-- Menu icons -->
-			<!-- Need to add statement that changes the image to the green version when it is on the corresponding page -->
-			<div class="flex flex-col items-center">
-				<img src="src/lib/menuIcons/homeWhite.svg" alt="Home Icon" style="height: 40px;">
-				<p>Home</p>
+
+			<div>
+				<a href=""  class="flex flex-col items-center">
+					<img src={$isHomePage ? "src/lib/menuIcons/homeGreen.svg" : "src/lib/menuIcons/homeWhite.svg"}
+					alt="Home Icon" 
+					style="height: 40px;"
+					class:active-icon={$isHomePage}>
+					
+					<p class:active-text={$isHomePage}>Home</p>
+				</a>
 			</div>
 
-			<div class="flex flex-col items-center">
-				<img src="src/lib/menuIcons/uploadWhite.svg" alt="Upload File Icon" style="height: 40px;">
-				<p>Upload Files</p>
+
+			<div>
+				<a href="" class="flex flex-col items-center">
+					<img src={$isUploadPage ? "src/lib/menuIcons/uploadGreen.svg" : "src/lib/menuIcons/uploadWhite.svg"} 
+					alt="Upload File Icon" 
+					style="height: 40px;">
+
+					<p class:active-text={$isUploadPage}>Upload Files</p>
+				</a>
 			</div>
 			
-			<div class="flex flex-col items-center">
-				<img src="src/lib/menuIcons/accountWhite.svg" alt="Account Icon" style="height: 40px;">
-				<p>Profile</p>
+
+			<div>
+				<a href="/src/routes/(app)/app/account"  class="flex flex-col items-center">
+					<img src={$isProfilePage ? "src/lib/menuIcons/accountGreen.svg" : "src/lib/menuIcons/accountWhite.svg"} 
+					alt="Account Icon" 
+					style="height: 40px;">
+					
+					<p class:active-text={$isProfilePage}>Profile</p>
+
+				</a>
 			</div>
 
 		</div>
 	</div>
 </header>
+
+
+<style>
+	p {
+		color: white;
+	}
+
+	.active-text {
+		color: #6CFF96;
+	}
+</style>
