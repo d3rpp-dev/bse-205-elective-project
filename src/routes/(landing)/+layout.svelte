3b50d8726816bafecc09c103ee5758f87478b867
Huddy Buddy <github@d3rpp.dev>
2024-11-20 09:17:35 +0000

    added privacy policy and made scrolling work nicely (#15)
    * added privacy policy and made scrolling work nicely
    
    * formatting

--- src/routes/(landing)/+layout.svelte
+++ src/routes/(landing)/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { LandingHeader } from "@/headers";
 	import { LandingFooter } from "@/footers";
-	import Main from "@/main.svelte";
 
 	const { data, children } = $props();
 	const { user } = data;
@@ -12,11 +11,9 @@
 <!-- div to add background to pages solely affected by the landing layout -->
 
 <div
-	class="h-full bg-cover bg-center"
+	class="h-auto min-h-landing-main bg-cover bg-center"
 	style="background-image: url('/backgroundsAndAssets/newLandingBackground.jpg');background-attachment: fixed; background-size: cover; background-position: center;"
 >
-	<Main class="h-landing-main">
-		{@render children()}
-	</Main>
+	{@render children()}
 </div>
 <LandingFooter />
